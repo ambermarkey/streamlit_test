57266064ddfd62da9b4ed0128ec285619555e7aa
amber markey <57023228+ambermarkey@users.noreply.github.com>
2022-03-03 10:17:02 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,17 @@
-print "Hello World!";
+# Load libraries
+import codecademylib3
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Read in the data
+students = pd.read_csv('test_data.csv')
+
+# Write equation for a line
+y = 9.85 * students.hours_studied + 43
+
+# Create the plot here: 
+
+
+#plt.plot(students.hours_studied, y)
+plt.show()
